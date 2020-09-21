95f728d86c76b5365b4bc9a89e2e10752c905c2d
Kleber De Menezes Thanasio *Kleberm* <kleberm@riachuelo.com.br>
2020-09-21 00:20:28 -0300

    Adding Authentication to LineUP API

--- app/routes/LineUpRoute.py
+++ app/routes/LineUpRoute.py
@@ -13,6 +13,8 @@ from app.routes.validations.LineUpCreateInputSchema import LineUpCreateInputSche
 from datetime import datetime
 from app.shared.Util import format_datetime
 
+from app.shared.Authentication import is_logged, is_admin
+
 auth = HTTPBasicAuth()
 
 create_line_up_schema = LineUpCreateInputSchema()
@@ -25,6 +27,7 @@ def new_line_up():
         error_list.append({k: v})
     if errors:
         return (jsonify({'errors': error_list}), 400)
+
     line_up = LineUp(waiting_line_id= req_data['waiting_line_id'],
                      customer_id= req_data['customer_id'],
                      status=0, # ENTROU NA FILA
@@ -33,7 +36,9 @@ def new_line_up():
     if line_up.query.filter(and_(LineUp.customer_id==req_data['customer_id'],LineUp.status < 3)).first() is not None:
         return (jsonify({'message': 'Customer already in an active Waiting Line'}), 400)
 
-    # user.hash_password(req_data['password'])
+    if not is_logged(): # TODO: VALIDAR SE O USUÁRIO PERTENCE A EMPRESA
+        return (jsonify({'message': 'Not Authorized' })), 401
+
     db.session.add(line_up)
     db.session.commit()
 
@@ -54,8 +59,6 @@ def get_next_customer():
     next_customer = line_up.query.filter(and_(LineUp.waiting_line_id==waiting_line_id,LineUp.status == 0)).order_by(db.asc('joined_at')).first()
     if not next_customer:
         return (jsonify({'message': 'Fila de espera vazia'}), 404)
-
-    # return (jsonify({'message': 'Customer already in an active Waiting Line'}), 400)
 
     response = flask.make_response(jsonify({ 'data': {
                                         'id': next_customer.id,
@@ -101,42 +104,3 @@ def call_customer(id):
                                         'status': line_up.status}}), 200)
     response.headers["Content-Type"] = "application/json"
     return response
-
-
-# @api.route('/api/companies/login')
-# @auth.login_required
-# def get_auth_token():
-#     token = g.user.generate_auth_token(600)
-
-#     response = flask.make_response({'token': token.decode('ascii'), 'duration': 600}, 200)
-#     response.headers["Content-Type"] = "application/json"
-#     response.set_cookie('token', token.decode('ascii'), secure=False)
-#     return response
-
-@auth.verify_password
-def verify_password(pid, password='password'):
-    # first try to authenticate by token
-    user = User.verify_auth_token(pid)
-    if not user:
-        # try to authenticate with username/password
-        user = User.query.filter_by(username=pid).first()
-        if not user or not user.verify_password(password):
-            return False
-    g.user = user
-    return True
-
-# @api.route('/api/companies/currentuser')
-# def get_current_line_up():
-#     if not is_logged():
-#         return (jsonify({'message': 'Not Authorized' })), 401
-#     response = flask.make_response({ 'data': {
-#                                         'id': g.user.id,
-#                                         'username': g.user.username, 
-#                                         'email': g.user.email,
-#                                         'phone_region': g.user.phone_region,
-#                                         'phone_number': g.user.phone_number,
-#                                         'status': g.user.status}}, 200)
-#     return response
-
-# def is_logged():
-#     return verify_password(request.cookies.get('token'))
