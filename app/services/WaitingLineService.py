b5f2c672538663ecbc08573c50bda11d3327d73c
kthanasio <kleber.thanasio@aluno.faculdadeimpacta.com.br>
2020-11-10 15:33:08 -0300

    Detalhes da fila

--- app/services/WaitingLineService.py
+++ app/services/WaitingLineService.py
@@ -8,6 +8,8 @@ from sqlalchemy import and_
 from datetime import datetime
 from app.shared.Util import format_datetime
 
+from app.models.LineUp import LineUp
+
 def save(data):
   name = data['name']
   waiting_line = WaitingLine(name=name,
@@ -38,15 +40,23 @@ def getAll():
   waiting_lines = WaitingLine.query.filter_by(company_id=g.user.company_id).all()
   if not waiting_lines:
       return (jsonify({'message': 'No Waiting Lines found'}), 200)
-  resp = {'data': []} 
+  resp = {'data': [],'summary': {}} 
+  lowest = None
   for waiting_line in waiting_lines:
+      line_up = LineUp()
+      total = line_up.query.filter(and_(LineUp.waiting_line_id==waiting_line.id,LineUp.status < 3)).count()
+      # if (lowest is None):
+      #   lowest = round(((datetime.now() - line_up.joined_at).total_seconds())/60)
       resp['data'].append( {  'id': waiting_line.id,
                               'company_id': waiting_line.company_id,
                               'name': waiting_line.name, 
                               'is_priority': waiting_line.is_priority,
                               'status': waiting_line.status,
                               'created_at': format_datetime(waiting_line.created_at),
-                              'updated_at': format_datetime(waiting_line.updated_at)})
+                              'updated_at': format_datetime(waiting_line.updated_at),
+                              'qty_total': total,
+                              'max_waiting_minutes': lowest}
+                              )
   response = flask.make_response(jsonify(resp), 200)
   
   response.headers["Content-Type"] = "application/json"
